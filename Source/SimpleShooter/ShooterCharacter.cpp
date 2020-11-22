17803a23086461a4b823cb4da9245a8b51f3f0eb
Marko Sarkanj <marko.sarkanj@gmail.com>
2020-11-22 17:25:56 +0100

    lecture 202. completed

--- Source/SimpleShooter/ShooterCharacter.cpp
+++ Source/SimpleShooter/ShooterCharacter.cpp
@@ -7,7 +7,7 @@
 AShooterCharacter::AShooterCharacter()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+    PrimaryActorTick.bCanEverTick = true;
 }
 
 // Called when the game starts or when spawned
